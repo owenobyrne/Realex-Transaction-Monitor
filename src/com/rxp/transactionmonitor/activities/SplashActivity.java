c9ae12086a649bcbe05348463538a24179fc778d
owenobyrne <owen.obyrne@iol.ie>
2012-10-04 00:36:55 +0100

    Splash screen

---
+++ src/com/rxp/transactionmonitor/activities/SplashActivity.java
@@ -0,0 +1,58 @@
+package com.rxp.transactionmonitor.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager.LayoutParams;
+
+import com.rxp.transactionmonitor.R;
+
+public class SplashActivity extends Activity {
+
+	protected boolean _active = true;
+	protected int _splashTime = 5000;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().addFlags(LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.splash);
+
+		// thread for displaying the SplashScreen
+		Thread splashTread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					int waited = 0;
+					while (_active && (waited < _splashTime)) {
+						sleep(100);
+						if (_active) {
+							waited += 100;
+						}
+					}
+				} catch (InterruptedException e) {
+					// do nothing
+				} finally {
+					finish();
+					startActivity(new Intent(
+							"com.rxp.transactionmonitor.Main"));
+					
+				}
+			}
+		};
+		splashTread.start();
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			_active = false;
+		}
+		return true;
+	}
+
+}
